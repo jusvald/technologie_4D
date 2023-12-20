04045de323be9dba0171c8bdfb1e7ab6b90a5e5d
jusvald vald <valderama.justin@iisgalvanimi.edu.it>
2023-12-20 15:21:45 +0000

    compito ngfor

---
+++ essercizio_for/src/app/lista/lista.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-elementi',
+  template: `
+    <h1 class="titolo">Gli Animali più Grandi del Mondo </h1>
+    <ul >
+      <li *ngFor="let elemento of elementi" class="elemento" >{{ elemento }}</li>
+      <br> <!-- Aggiungiamo un <br> per separare gli elementi -->
+    </ul>
+  `,
+  styles: [`
+  .elemento {
+    font-size: 50px;
+  }
+  .titolo{
+    font-size: 70px;
+  }
+`],
+})
+export class ListaComponent {
+  elementi: string[] = ['Balenottera Azzurra', ' Balenottera Comune', 'Capodoglio', 'Calamaro Colossale'];
+}
