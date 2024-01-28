8bb52e82628b418d5af51c790a3d78f21a43c577
jusvald vald <valderama.justin@iisgalvanimi.edu.it>
2024-01-28 15:40:42 +0000

    compito con routing

---
+++ Prova_Angular_Routing/src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AnimalsComponent } from './animals/animals.component';
+import { FruitsComponent } from './fruits/fruits.component';
+
+const routes: Routes = [ {path: 'animals', component: AnimalsComponent} ,{path: 'fruits', component: FruitsComponent}, {path: '', redirectTo: '/animals', pathMatch: 'full'},];////qui tutti i route
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
